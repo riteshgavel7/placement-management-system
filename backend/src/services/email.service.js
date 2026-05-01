ee9db69cf18c85fe6c8c63272ab6e66894a0e5ff
ritesh gavel <riteshgavel777@gmail.com>
2026-05-01 23:16:50 +0530

    Update email.service.js

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -2,18 +2,14 @@ const nodemailer = require("nodemailer");
 
 exports.sendMail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, 
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    },
-    family: 4 
+    }
   });
 
   await transporter.sendMail({
-    from: `"Placement Portal" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     text
